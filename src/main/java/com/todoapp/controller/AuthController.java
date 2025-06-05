d1b3e8213f3ae6ab57bbbf74e9f4d54f947fc47d
mat1520 <azaazay09@gmail.com>
2025-06-04 19:33:46 -0500

    Add Maven Wrapper for deployment on Render

--- src/main/java/com/todoapp/controller/AuthController.java
+++ src/main/java/com/todoapp/controller/AuthController.java
@@ -1,17 +1,15 @@
 package com.todoapp.controller;
 
-import com.todoapp.model.User;
-import com.todoapp.service.UserService;
-import jakarta.validation.constraints.NotBlank;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.todoapp.model.User;
+import com.todoapp.service.UserService;
 
 @Controller
 public class AuthController {
